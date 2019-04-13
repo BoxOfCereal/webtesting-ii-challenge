06f7030b48ed6ad3154052529dd4ed0597365e1b
Noah <noah.stoeckel@gmail.com>
2019-04-13 13:15:07 -0400

    Writes Tests To Check If Balls Reset

--- baseball/src/Interface/Interface.test.js
+++ baseball/src/Interface/Interface.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDom from "react-dom";
-import { render, fireEvent } from "react-testing-library";
+import { render, fireEvent, cleanup } from "react-testing-library";
 import "jest-dom/extend-expect";
 
 import Interface from "./Interface";
@@ -17,6 +17,21 @@ describe("The Interface Component", () => {
 
     const { getByTestId, getByText } = render(<Interface />);
     const button = getByText("ball");
+    fireEvent.click(button);
+
+    const ballCount = getByTestId("ball-count").textContent;
+
+    expect(ballCount).toBe(ballsAfterClick);
+    cleanup();
+  });
+  it("resets balls after four balls", () => {
+    const ballsAfterClick = "0";
+
+    const { getByTestId, getByText } = render(<Interface />);
+    const button = getByText("ball");
+    fireEvent.click(button);
+    fireEvent.click(button);
+    fireEvent.click(button);
     fireEvent.click(button);
 
     const ballCount = getByTestId("ball-count").textContent;
